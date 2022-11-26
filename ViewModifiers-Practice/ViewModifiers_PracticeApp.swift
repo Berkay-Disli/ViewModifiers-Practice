1561da12855d4c7ad8ec1f25656c5f9f987c61b6
Berkay-Disli <berkaydisli.work@gmail.com>
2022-11-26 21:08:42 +0300

    Initial Commit

---
+++ ViewModifiers-Practice/ViewModifiers_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifiers_PracticeApp.swift
+//  ViewModifiers-Practice
+//
+//  Created by Berkay Disli on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifiers_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
